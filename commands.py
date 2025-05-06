1248834e53d262d801d98e6348fe0d69c5224f76
xDMBDx <68405697+xDMBDx@users.noreply.github.com>
2025-05-06 13:22:31 -0500

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,7 @@
+commands = {
+    "farm_cycle": ["/work", "/tip", "/overtime", "/clean", "/daily"],
+    "gambling": ["/gamble", "/slots", "/roulette", "/coinflip"],
+    "economy_boost": ["/vote", "/boost"],
+    "management": ["/upgrade", "/advertise", "/expand", "/rename"],
+    "sauce": ["/sauce market", "/sauce buy", "/sauce sell"],
+}
